86377da5130366b9fb57d2edbbcf3950b0afc7f5
Aayushi <aayushi.panchal@mobavenue.com>
2026-07-02 16:56:51 +0530

    fb_401_to_loginpage

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { clearToken } from './auth'
 
 const api = axios.create({
   baseURL: 'http://localhost:8000/api/v1',
@@ -11,5 +12,18 @@ api.interceptors.request.use((config) => {
   }
   return config
 })
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      clearToken()
+      if (typeof window !== 'undefined' && window.location.pathname !== '/login') {
+        window.location.replace('/login')
+      }
+    }
+    return Promise.reject(error)
+  }
+)
 
 export default api
